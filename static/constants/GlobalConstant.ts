8f110b6ea6736e051859a7e544e29a07e28a2406
ppyakurel-wyn <ppyakurel@wynshop.com>
2022-12-19 01:08:57 +0545

    static value updated by global constant

--- static/constants/GlobalConstant.ts
+++ static/constants/GlobalConstant.ts
@@ -1,3 +1,19 @@
 export const GLOBAL_CONSTANT = {
   USER: 'Pawan Pyakurel',
+  TABLE_DATA: [
+    {
+      dataKey: 'account',
+      value: '',
+    },
+    {
+      dataKey: 'chain ID',
+      value: '',
+    },
+    {
+      dataKey: 'balance',
+      value: '',
+    },
+    { dataKey: 'block no', value: '' },
+  ],
+  CONNECT_MESSAGE: `Wallet is not Connected. Plese Press <b>'Connect'</b> button below.`,
 };
